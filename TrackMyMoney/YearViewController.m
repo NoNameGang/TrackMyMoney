75227c8662405c251064094c692de8b5c4fe5c5d
he110world <he110world@126.com>
2013-10-31 17:42:26 +0800

    Save image file

--- TrackMyMoney/YearViewController.m
+++ TrackMyMoney/YearViewController.m
@@ -72,8 +72,12 @@
     [self.tableView selectRowAtIndexPath:[NSIndexPath indexPathForRow:(int)thisDateCf.year - (int)firstDateCf.year inSection:0] animated:YES scrollPosition:UITableViewScrollPositionMiddle];
     
     // FUCK: CF Ref need to be released!!
-    //CFRelease(firstDateRef);
-    //CFRelease(timeZoneRef);
+    if(firstDateRef){
+        CFRelease(firstDateRef);
+    }
+    if(timeZoneRef){
+        CFRelease(timeZoneRef);
+    }
 }
 
 - (void)didReceiveMemoryWarning
